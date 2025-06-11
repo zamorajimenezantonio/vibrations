5e62c512b5a75ed03f4756b962edef503a5df979
Antonio Zamora Jimenez <azj@iac.es>
2025-06-11 13:21:26 +0100

    updating the matlab function

--- calculateBode.m
+++ calculateBode.m
@@ -1,8 +1,8 @@
-function [f,mag_vect]=calculateBode(w0,winc,wend)
+function [f,mag_vect]=calculateBode(w0,winc,wend,input,output)
 
-    A = cell2mat(struct2cell(load('A.mat')));
-    B = cell2mat(struct2cell(load('B.mat')));
-    C = cell2mat(struct2cell(load('C.mat')));
+    A = cell2mat(struct2cell(load(strcat('A','_',num2str(input),'_',num2str(output),'.mat'))));
+    B = cell2mat(struct2cell(load(strcat('B','_',num2str(input),'_',num2str(output),'.mat'))));
+    C = cell2mat(struct2cell(load(strcat('C','_',num2str(input),'_',num2str(output),'.mat'))));
     D=[[0]];
     
     sys1 = ss(A,B,C,D);
@@ -12,7 +12,7 @@ function [f,mag_vect]=calculateBode(w0,winc,wend)
     f=wout/(2*pi);
     %loglog(f,mag_vect);
     bode_data=[f,mag_vect]
-    csvwrite('bode_data.csv',bode_data)
+    csvwrite(strcat('bode_data','_',num2str(input),'_',num2str(output),'.csv'),bode_data)
     
 end
 
